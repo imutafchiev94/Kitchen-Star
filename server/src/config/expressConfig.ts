52cd4d95f8bda077c350cf25ab91bb6a1cddb1cb
Ivaylo Mutafchiev <69365693+imutafchiev94@users.noreply.github.com>
2024-09-30 23:43:34 +0300

    Added isGuest and isAuthenticated middlewares

--- server/src/config/expressConfig.ts
+++ server/src/config/expressConfig.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import auth from '../middlewares/auth';
 
 
 function setupExpress(app: any) {
@@ -10,7 +9,6 @@ function setupExpress(app: any) {
     app.use(express.json());
     app.use(express.urlencoded({extended: true}));
     app.use(cookieParser());
-    app.use(auth());
 }
 
 export default setupExpress;
---
+++ server/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export default (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const token = req.cookies[process.env.COOKIE_SESSION_NAME || 'defaultCookieName'];
+
+        if (!token) {
+            return res.status(401).json({ message: 'Access Denied. No token provided.' });
+        }
+
+        const secret = process.env.USER_SESSION_SECRET || 'defaultUserSessionSecret';
+        const decoded = jwt.verify(token, secret) as any;
+
+        if(!decoded.user) {
+            return res.status(403).json({ message: 'Access Denied. Logged users only.'});
+        }
+
+        next();
+        
+    } catch(error) {
+        let message = 'Unknown Error';
+        if(error instanceof Error) {
+            message = error.message;
+        }
+        return res.status(400).json({ message: 'Invalid token.', error: message });
+    }
+}
