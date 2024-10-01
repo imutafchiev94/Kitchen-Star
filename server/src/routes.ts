28270a0bfe7a20e98311d979e210ad1f1636a6ec
Ivaylo Mutafchiev <69365693+imutafchiev94@users.noreply.github.com>
2024-10-01 23:15:40 +0300

    added isAdmin middleware to admin routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 import homeController from './controllers/homeController';
 import authController from './controllers/authController';
 import adminRouter from './admin/adminRoutes';
+import isAdmin from "./middlewares/isAdmin";
 const router = Router();
 
 router.use('/', homeController);
-router.use('/admin', adminRouter);
+router.use('/admin', isAdmin, adminRouter);
 router.use('/auth', authController);
 
 export default router;
