df586f8c9d0c82d9432452aa676e7bfee6b1257b
Ivaylo Mutafchiev <i.mutafchiev94@outlook.com>
2026-07-14 23:56:06 +0300

    Renamed the project to Chef-Star

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Kitchen-Star
+# Chef-Star
 Modern full-stack e-commerce platform for kitchenware built with Angular, NestJS, TypeScript, PostgreSQL, Docker and CI/CD.
--- chef-star/src/app/app.ts
+++ chef-star/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('kitchen-star');
+  protected readonly title = signal('chef-star');
 }
